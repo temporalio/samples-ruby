444222dc5a50ed102670ccd1f4c64d05d8804160
Thomas Hardy <thomas.hardy@temporal.io>
2025-10-09 12:07:49 -0700

    Add basic test for eager workflow start sample

---
+++ test/eager_wf_start/eager_workflow_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'test'
+require 'eager_wf_start/eager_workflow'
+require 'securerandom'
+require 'temporalio/testing'
+require 'temporalio/worker'
+
+module EagerWfStart
+  class EagerWorkflowTest < Test
+    def test_workflow
+      # Run test server until completion of the block
+      Temporalio::Testing::WorkflowEnvironment.start_local do |env|
+        # Run worker until completion of the block
+        worker = Temporalio::Worker.new(
+          client: env.client,
+          task_queue: "tq-#{SecureRandom.uuid}",
+          activities: [GreetingActivity],
+          workflows: [EagerWorkflow]
+        )
+        worker.run do
+          # Run workflow with eager start
+          result = env.client.execute_workflow(
+            EagerWorkflow,
+            'Temporal',
+            id: "wf-#{SecureRandom.uuid}",
+            task_queue: worker.task_queue,
+            request_eager_start: true
+          )
+          assert_equal('Hello, Temporal!', result)
+        end
+      end
+    end
+  end
+end
