27bcdc0cf74098aa6b3ef89ea4d890f734c7cd30
Thomas Hardy <thomas.hardy@temporal.io>
2025-10-10 12:41:30 -0700

    Add eager started assertion to test

--- test/eager_workflow_start/eager_workflow_test.rb
+++ test/eager_workflow_start/eager_workflow_test.rb
@@ -10,7 +10,9 @@ module EagerWorkflowStart
   class EagerWorkflowTest < Test
     def test_workflow
       # Run test server until completion of the block
-      Temporalio::Testing::WorkflowEnvironment.start_local do |env|
+      Temporalio::Testing::WorkflowEnvironment.start_local(
+        dev_server_download_version: 'latest'
+      ) do |env|
         # Run worker until completion of the block
         worker = Temporalio::Worker.new(
           client: env.client,
@@ -19,15 +21,19 @@ module EagerWorkflowStart
           workflows: [EagerWorkflow]
         )
         worker.run do
-          # Run workflow with eager start
-          result = env.client.execute_workflow(
+          # Start workflow with eager start
+          handle = env.client.start_workflow(
             EagerWorkflow,
             'Temporal',
             id: "wf-#{SecureRandom.uuid}",
             task_queue: worker.task_queue,
             request_eager_start: true
           )
-          assert_equal('Hello, Temporal!', result)
+          assert_equal('Hello, Temporal!', handle.result)
+
+          # Verify workflow was eagerly executed
+          started_event = handle.fetch_history_events.find(&:workflow_execution_started_event_attributes)
+          assert(started_event.workflow_execution_started_event_attributes.eager_execution_accepted)
         end
       end
     end
